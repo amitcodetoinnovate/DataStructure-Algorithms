34b1ad833d33136d7ebe1821f51c54c7e665fc1d
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-07-22 01:03:09 +0530

    Leetcode - 26. Remove Duplicates from Sorted Array

---
+++ Algorithms/Leetcode/RemoveDuplicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Leetcode
+{
+    public class RemoveDuplicate
+    {
+        public static int RemoveDuplicatesSol(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[k] != nums[i])
+                    nums[++k] = nums[i];
+            }
+            return k + 1;
+        }
+    }
+}
--- DataStructuresAndAlgorithms.Test/LeetCodeTest.cs
+++ DataStructuresAndAlgorithms.Test/LeetCodeTest.cs
@@ -1,6 +1,8 @@
 using System;
 using Algorithms;
+using Algorithms.Leetcode;
 using Xunit;
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
 namespace DataStructureAndAlgorithms.Test
 {
@@ -10,11 +12,19 @@ namespace DataStructureAndAlgorithms.Test
         [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 7)]
         [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4)]
         [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
-        public void StockBuySell(int[] prices,int ans)
+        public void StockBuySell_Test(int[] prices,int ans)
         {
             Assert.Equal(StockBuySellII.MaxProfitBF(prices),ans);
             Assert.Equal(StockBuySellII.MaxProfitBF(prices),ans);
             Assert.Equal(StockBuySellII.MaxProfitOptimized(prices),ans);
+        }
+        
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void RemoveDuplicate_Test(int[] nums, int ans)
+        {
+            Assert.Equal(RemoveDuplicate.RemoveDuplicatesSol(nums), ans);
         }
     }
 }
