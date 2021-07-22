ffa785f994bb860491c6c9d5ad954f422237daba
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-07-22 19:14:58 +0530

    Algorithms I - Added Heap Sort

---
+++ AlgorithmsPartI/PriorityQueues/HeapSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AlgorithmsPartI
+{
+    public class HeapSort
+    {
+        public static void Sort<T>(T[] pq) where T : IComparable<T>
+        {
+            int n = pq.Length;
+            int k;
+            for (k = n / 2; k >= 1; k--)
+            {
+                Sink(pq, k, n);
+            }
+
+            k = n;
+            while (k > 1)
+            {
+                Exch(pq, 1, k--);
+                Sink(pq, 1, k);
+            }
+        }
+
+        private static void Sink<T>(T[] pq, int k, int n) where T : IComparable<T>
+        {
+            while (2 * k <= n)
+            {
+                int j = 2 * k;
+                if (j < n && Less(pq, j, j + 1)) j++;
+                if (!Less(pq, k, j))
+                    break;
+                Exch(pq, k, j);
+                k = j;
+            }
+        }
+
+        private static bool Less<T>(T[] pq, int i, int j) where T : IComparable<T>
+        {
+            return pq[i - 1].CompareTo(pq[j - 1]) < 0;
+        }
+
+        private static void Exch<T>(T[] pq, int i, int j) where T : IComparable<T>
+        {
+            T swap = pq[i - 1];
+            pq[i - 1] = pq[j - 1];
+            pq[j - 1] = swap;
+        }
+    }
+}
--- DataStructuresAndAlgorithms.Test/PriorityQueues/PriorityQueueHeapTest.cs
+++ DataStructuresAndAlgorithms.Test/PriorityQueues/PriorityQueueHeapTest.cs
@@ -1,4 +1,6 @@
-﻿using AlgorithmsPartI;
+﻿using System;
+using System.Collections;
+using AlgorithmsPartI;
 using Microsoft.VisualBasic.CompilerServices;
 using Xunit;
 
@@ -32,6 +34,19 @@ namespace DataStructuresAndAlgorithms.Test.PriorityQueues
                 pq.Insert(num);
             }
             Assert.Equal(pq.Min(), ans);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 7, 1, 5, 3, 6, 4 })]
+        [InlineData(new int[] { 2, 8, 1, 3, 6, 7, 5, 4 })]
+        [InlineData(new int[] { 10, 14, 28, 11, 7, 16, 30, 50, 25, 18 })]
+        public void HeapSort_Test(int[] nums)
+        {
+            var output = (int[])nums.Clone();
+            Array.Sort(output);
+            HeapSort.Sort(nums);
+            
+            Assert.Equal(nums, output);
         }
     }
 }
