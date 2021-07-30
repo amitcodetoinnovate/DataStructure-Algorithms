d6c04a9c533ae74cc4e7a5b6a0892b1e7326e60a
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-07-31 03:11:26 +0530

    Leetcode - 3. Longest Substring Without Repeating Characters

---
+++ Algorithms/Leetcode/3.LongestNonRepeating.cs
@@ -0,0 +1,37 @@
+﻿//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+using System;
+using System.Collections.Generic;
+
+
+namespace Algorithms.Leetcode
+{
+    public class LongestNonRepeatingSubsequence
+    {
+        public static int Solve(string s)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int max = Int32.MinValue;
+            int i = 0, j = 0;
+            while (i < s.Length && j < s.Length)
+            {
+                if (!map.ContainsKey(s[i]) || (map.ContainsKey(s[i]) && map[s[i]] < j))
+                {
+                    max = Math.Max(max, i - j + 1);
+                    if (!map.ContainsKey(s[i]))
+                        map.Add(s[i], i);
+                    else
+                        map[s[i]] = i;
+                    i++;
+                }
+                else
+                {
+                    j = map[s[i]] + 1;
+                    map[s[i]] = i;
+                    i++;
+
+                }
+            }
+            return max;
+        }
+    }
+}
