149d7999803a27057dd30b81ef6f369dfa603516
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-08-12 01:50:10 +0530

    Leetcode: 21. Merge Two Sorted Lists - Easy

--- Algorithms/Leetcode/2.AddTwoNumbers.cs
+++ Algorithms/Leetcode/2.AddTwoNumbers.cs
@@ -1,9 +1,5 @@
-﻿using DataStructures;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//https://leetcode.com/problems/add-two-numbers/
+using DataStructures;
 
 namespace Algorithms.Leetcode
 {
