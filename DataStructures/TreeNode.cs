6389c263ac7ae42619963662a446fc0427753b50
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-08-20 01:55:39 +0530

    Leetcode: 98. Validate Binary Search Tree - Medium

---
+++ DataStructures/TreeNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
