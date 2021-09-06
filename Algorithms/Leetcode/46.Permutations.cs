ae7da182c5a02f53be4a7703406cb1005e81aac7
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-09-06 23:09:25 +0530

    Leetcode: 46. Permutation - Medium

---
+++ Algorithms/Leetcode/46.Permutations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Leetcode
+{
+    public class Permutations
+    {
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            PermuteHelper(new List<int>(nums), new List<int>(), res);
+            return res;
+        }
+        private static void PermuteHelper(List<int> nums, List<int> choosen, IList<IList<int>> res)
+        {
+            if (nums.Count == 0)
+                res.Add(new List<int>(choosen));
+            else
+            {
+                
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    choosen.Add(nums[i]);
+                    nums.RemoveAt(i);
+                    PermuteHelper(nums, choosen, res);
+                    nums.Insert(i, choosen[choosen.Count - 1]);
+                    choosen.RemoveAt(choosen.Count - 1);
+                }
+
+            }
+        }
+    }
+}
