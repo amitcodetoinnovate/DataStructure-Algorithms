a30972ba0faec8626c3e169c048e1f849fada526
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-09-29 12:43:25 +0530

    133. Clone Graph - Medium

---
+++ DataStructures/GraphNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class GraphNode
+    {
+        public int val;
+        public IList<GraphNode> neighbors;
+
+        public GraphNode()
+        {
+            val = 0;
+            neighbors = new List<GraphNode>();
+        }
+
+        public GraphNode(int val)
+        {
+            this.val = val;
+            neighbors = new List<GraphNode>();
+        }
+
+        public GraphNode(int val, List<GraphNode> neighbors)
+        {
+            this.val = val;
+            this.neighbors = neighbors;
+        }
+    }
+}
