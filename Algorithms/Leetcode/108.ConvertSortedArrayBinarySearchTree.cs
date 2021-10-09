ab61203908b32bd475f77614aa3a66a93f2f0b93
Amit Kumar Verma <Amit.Verma@ivycomptech.com>
2021-10-09 17:55:51 +0530

    Added new Leetcode problems

---
+++ Algorithms/Leetcode/108.ConvertSortedArrayBinarySearchTree.cs
@@ -0,0 +1,21 @@
+﻿using DataStructures;
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+namespace Algorithms.Leetcode
+{
+    public class ConvertSortedArrayBinarySearchTree
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return SortedHelper(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode SortedHelper(int[] nums, int lo, int hi)
+        {
+            if (lo > hi) return null;
+            if (lo == hi) return new TreeNode(nums[lo]);
+            int mid = lo + (hi - lo) / 2;
+            return new TreeNode(nums[mid], SortedHelper(nums, lo, mid - 1), SortedHelper(nums, mid + 1, hi));
+        }
+    }
+}
---
+++ Algorithms/Leetcode/950.RevealCardsInIncreasingOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+// https://leetcode.com/problems/reveal-cards-in-increasing-order/
+
+namespace Algorithms.Leetcode
+{
+    public class RevealCardsInIncreasingOrder
+    {
+        public static int[] Solve(int[] deck)
+        {
+            Queue<int> q = new Queue<int>();
+            Array.Sort(deck);
+            int j = deck.Length - 1;
+            q.Enqueue(deck[j--]);
+            while (j > -1)
+            {
+                q.Enqueue(q.Dequeue());
+                q.Enqueue(deck[j--]);
+            }
+
+            for (int i = deck.Length - 1; i > -1; i--)
+                deck[i] = q.Dequeue();
+            return deck;
+
+        }
+    }
+}
